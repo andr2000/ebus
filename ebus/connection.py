b356ae4be24122a796254f95eca58fff102d3e80
Daniel Jakschik <daniel.jakschik@commsolid.com>
2019-12-19 23:01:32 +0100

    Extend connection implementation.

--- ebus/connection.py
+++ ebus/connection.py
@@ -8,8 +8,9 @@ class Connection:
     Keyword Args:
         host (str): Hostname or IP
         port (int): Port
+        autoconnect (bool): Automatically connect and re-connect
 
-    >>> async def app():  # doctest: +SKIP
+    >>> async def app():
     ...    c = Connection()
     ...    await c.connect()
     ...    line = await c.read('WaterPressure press', circuit='bai')
@@ -21,28 +22,72 @@ class Connection:
     ...        print(line)
     """
 
-    def __init__(self, host='127.0.0.1', port=8888):
+    def __init__(self, host='127.0.0.1', port=8888, autoconnect=False):
         self._host = host
         self._port = port
+        self._autoconnect = autoconnect
         self._reader, self._writer = None, None
 
+    @property
+    def host(self):
+        """Host."""
+        return self._host
+
+    @property
+    def port(self):
+        """Port."""
+        return self._port
+
+    @property
+    def autoconnect(self):
+        """Automatically connect and re-connect."""
+        return self._autoconnect
+
     async def connect(self):
-        """Establish connection (required before first communication)."""
+        """
+        Establish connection (required before first communication).
+
+        Raises:
+            IOError: If connection cannot be established
+        """
         self._reader, self._writer = await asyncio.open_connection(self._host, self._port)
 
     async def disconnect(self):
-        """Disconnect."""
-        self._writer.close()
-        await self._writer.wait_closed()
-        self._reader, self._writer = None, None
+        """Disconnect if not already done."""
+        if self._writer:
+            self._writer.close()
+            await self._writer.wait_closed()
+            self._reader, self._writer = None, None
+
+    def is_connected(self):
+        """
+        Return `True` if connection is established.
+
+        This does not check if the connection is still usable.
+        """
+        return self._writer and not self._writer.is_closing()
 
     async def send(self, message):
-        """Send `message`."""
+        """
+        Send `message`.
+
+        Raises:
+            IOError: If connection is broken or cannot be established (`autoconnect==True`)
+            NotConnectedError: If not connected (`autoconnect==False`)
+        """
+        await self._ensure_connection()
         self._writer.write(f"{message}\n".encode())
         await self._writer.drain()
 
     async def receive(self):
-        """Receive one line."""
+        """
+        Receive one line.
+
+        Raises:
+            IOError: If connection is broken or cannot be established (`autoconnect==True`)
+            NotConnectedError: If not connected (`autoconnect==False`)
+        """
+        await self._ensure_connection()
         line = await self._reader.readline()
         line = line.decode('utf-8').rstrip()
         if line == "ERR: shutdown":
@@ -51,7 +96,15 @@ class Connection:
         return line
 
     async def read(self, name, field=None, circuit=None, ttl=None):
-        """Read `name` extracting `field` from `circuit` not older than `ttl` seconds."""
+        """
+        Read `name` extracting `field` from `circuit` not older than `ttl` seconds.
+
+        Raises:
+            Error: In case of an unknown command or command argument (response contains `ERR`)
+            IOError: If connection is broken or cannot be established (`autoconnect==True`)
+            NotConnectedError: If not connected (`autoconnect==False`)
+        """
+        await self._ensure_connection()
         response = await self._request('read', [
             ('-m ', ttl),
             ('-c ', circuit),
@@ -61,7 +114,15 @@ class Connection:
         return response
 
     async def write(self, name, circuit, value):
-        """Write `value` to `name` in `circuit`."""
+        """
+        Write `value` to `name` in `circuit`.
+
+        Raises:
+            Error: In case of an unknown command or command argument (response contains `ERR`)
+            IOError: If connection is broken or cannot be established (`autoconnect==True`)
+            NotConnectedError: If not connected (`autoconnect==False`)
+        """
+        await self._ensure_connection()
         await self._request('write', [
             ('-c ', circuit),
             ('', name),
@@ -70,12 +131,20 @@ class Connection:
 
     async def start_listening(self, verbose=False):
         """Start Listening."""
+        await self._ensure_connection()
         command = 'listen -v' if verbose else 'listen'
         await self.send(command)
         # consume 'listen started'
         await self.receive()
         await self.receive()
         await self.receive()
+
+    async def _ensure_connection(self):
+        if not self._writer or self._writer.is_closing():
+            if self._autoconnect:
+                await self.connect()
+            else:
+                raise NotConnectedError()
 
     async def _request(self, command, options):
         args = ''.join([f'{option} {value} '
@@ -92,6 +161,9 @@ class Connection:
             raise CommandError(line)
 
 
-class Error(RuntimeError):
+class NotConnectedError(OSError):
+    """Connection is not established."""
 
+
+class Error(RuntimeError):
     """Exception raised in case of an error."""
